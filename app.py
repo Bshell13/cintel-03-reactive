2e36dad58a399fbb71c94475e19c168006705303
Brandon <155925770+Bshell13@users.noreply.github.com>
2024-03-17 19:22:54 -0500

    reactive calculation
    Added a working reactive calculation where the user can select a specific penguin species.

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ with ui.navset_card_tab(id="tab"):
 
 @reactive.calc
 def filtered_data():
-    return penguins_df
+    return penguins_df[penguins_df["species"].isin(input.selected_species_list())]
